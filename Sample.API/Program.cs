33e5335ba2f7c28cc8cbb2162b621759dfc4218d
Guilherme Lino <guilhermelinosp@gmail.com>
2023-08-30 11:05:20 -0300

    fead: add caching in memory

--- Sample.API/Program.cs
+++ Sample.API/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Sample.API.Data;
+using Sample.API.Services;
+using System.Runtime.Caching;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
 
+builder.Services.AddSingleton<ObjectCache>(MemoryCache.Default);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(configuration["ConnectionStrings:PostgreSQL"]));
+builder.Services.AddScoped<ICacheService, CacheService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +24,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ Sample.API/Services/CacheService.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.Caching;
+
+namespace Sample.API.Services
+{
+    public class CacheService : ICacheService
+    {
+        private readonly ObjectCache _cache;
+
+        public CacheService(ObjectCache cache)
+        {
+            _cache = cache;
+        }
+
+        public T? Get<T>(string key)
+        {
+            try
+            {
+                return (T)_cache.Get(key);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Set<T>(string key, T value, DateTimeOffset expirationTime)
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(key)) _cache.Set(key, value, expirationTime);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Remove(string key)
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(key)) _cache.Remove(key);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
